d7248f6e55728c4f7128f66311634abd56a612cd
Flavia <flavia.brogle@students.fhnw.ch>
2021-02-23 21:21:24 +0100

    Add RGB Sliders

--- src/gui/RGBSliders.java
+++ src/gui/RGBSliders.java
@@ -1,13 +1,19 @@
 package gui;
 
+import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import logic.ColorPickerModel;
 
 public class RGBSliders extends VBox implements ViewInit {
     
-    public RGBSliders(ColorPickerModel model) {
-        init();
+    Slider rSlider;
+    Slider gSlider;
+    Slider bSlider;
+    private ColorPickerModel model;
     
+    public RGBSliders(ColorPickerModel model) {
+        this.model = model;
+        init();
     }
     
     @Override
@@ -17,12 +23,16 @@ public class RGBSliders extends VBox implements ViewInit {
     
     @Override
     public void initializeControls() {
-    
+        rSlider = new Slider(0, 1, 0);
+        gSlider = new Slider(0, 1, 0);
+        bSlider = new Slider(0, 1, 0);
+        
+        
     }
     
     @Override
     public void layoutControls() {
-    
+        this.getChildren().addAll(rSlider, gSlider, bSlider);
     }
     
     @Override
@@ -37,6 +47,8 @@ public class RGBSliders extends VBox implements ViewInit {
     
     @Override
     public void setupBindings() {
-    
+        rSlider.valueProperty().bindBidirectional(model.rProperty());
+        gSlider.valueProperty().bindBidirectional(model.gProperty());
+        bSlider.valueProperty().bindBidirectional(model.bProperty());
     }
 }
